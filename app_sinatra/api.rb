b275826303d41b9c80da741a3a2969b5ec88691d
Ondrej Bartas <ondrej@bartas.cz>
2012-02-16 08:23:25 +0100

    [ADD] Basic api methods

--- app_sinatra/api.rb
+++ app_sinatra/api.rb
@@ -1,4 +1,37 @@
 # -*- encoding : utf-8 -*-
 class AskmeSinatra < Sinatra::Base
   
+  def render_output name = nil, items = nil
+    content_type :json
+    output = {:status => "ok", :message => "ok", :executed_at => Time.now.strftime("%Y-%m-%d %H:%M:%S") }
+    output[name] = items unless name
+    return JSON.pretty_generate(output)
+  end
+  
+  #Get all messages
+  get_or_post "/messages" do
+    #Code for getting messages goes there, it should be automatically search too
+    items = []
+    render_output "messages", items
+  end
+  
+  #Create message
+  get_or_post "/message/create" do
+    #Code for creating messsage goes there
+    render_output
+  end
+  
+  #Get all tags
+  get_or_post "/tags" do
+    #Code for getting tags goes there, it should be automatically search too
+    items = []
+    render_output "tags", items
+  end
+  
+  #Create tag
+  get_or_post "/tag/create" do
+    #Code for creating tag goes there
+    render_output
+  end
+  
 end
