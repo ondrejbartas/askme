9d61dc76ca6a3471d94217be79b59d7d672514cf
Radim Polasek <rpolasek@gmail.com>
2012-02-29 19:15:21 +0100

    the elasticsearch api has been modified to fit the message create/find model and the elasticsearch adapter

--- app_sinatra/api.rb
+++ app_sinatra/api.rb
@@ -1,15 +1,10 @@
 # -*- encoding : utf-8 -*-
 class AskmeSinatra < Sinatra::Base
   
-  def get_id_from_url id
-    return id.scan(/\d+/).first.to_i if id && id.size > 0  
-    return nil
-  end
-  
   def render_output name = nil, items = nil
     content_type :json
     output = {:status => "ok", :message => "ok", :executed_at => Time.now.strftime("%Y-%m-%d %H:%M:%S") }
-    output[name] = items unless name
+    output[name] = items unless name.nil?
     return JSON.pretty_generate(output)
   end
 
@@ -40,35 +35,46 @@ class AskmeSinatra < Sinatra::Base
     render_output
   end
   
-  #Get all messages
-  get %r{/messages(\z|/[\d]*)} do |id|
-    id = get_id_from_url id
-    #Code for getting messages goes there, it should be automatically search too
-    items = []
-    items << id
-    render_output "messages", items
+  # for the elasticsearch
+
+  # get a message for the appropriate message id
+  get "/message/:id" do |id|
+    # TODO: exception
+    msg = MessageFindModel.new :ids => [id.to_i]
+    result = msg.find
+
+    render_output 'found_message', result[0]
+  end
+
+  # get message(s) (complex search query)
+  get '/message' do
+    # TODO: exception
+    msg = MessageFindModel.new(params)
+    result = msg.find
+
+    render_output "found_message(s)", result
+  end
+
+  # create a message
+  post '/message' do
+    message = params
+    message['id'] = RedisID.get(:message)
+
+    # TODO: exception
+    msg = MessageCreateModel.new(message)
+    result = msg.save
+
+    render_output "saved_message", result
   end
   
-  #Create message
-  post "/messages" do
-    message_id = RedisID.get(:message)
-    #Code for creating messsage goes there
-    render_output
+  # ++rank
+  post "/rank/inc/:id" do |id|
+    # TODO: call to increase a rank for message id
   end
-  
-  #Get all tags
-  get %r{/tags(\z|/[\d]*)} do |id|
-    id = get_id_from_url id
-    #Code for getting tags goes there, it should be automatically search too
-    items = []
-    render_output "tags", items
+
+  # --rank
+  post "/rank/dec/:id" do |id|
+    # TODO: call to descrease a rank for message id
   end
-  
-  #Create tag
-  post "/tag/create" do
-    tag_id = RedisID.get(:tag)
-    #Code for creating tag goes there
-    render_output
-  end
-  
+
 end
